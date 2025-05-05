32ffa4c324e9a34d9a5a27e0f0b3bd8c56803729
leylandoptony <Leyland.Yang@optonyusa.com>
2025-05-05 12:58:09 -0700

    first commit

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  { "rebelot/kanagawa.nvim" },
+  { "NLKNguyen/papercolor-theme" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rose-pine/neovim" },
+  { "tiagovla/tokyodark.nvim" },
+  { "sainnhe/everforest" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
